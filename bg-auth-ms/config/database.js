bedb2faddec2304509f0d554e575107a7dac89cd
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-03-13 17:55:55 -0500

    Se agrega el modelo y la configuración de la base de datos

---
+++ bg-auth-ms/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret':'lakjsdf8734bxiwue%&ASDYHB$&/THNMDFYUYGBNMKIUYiekdxnrtyucix',
+    'database': 'mongodb+srv://root:Sofka123*@bg-auth-cl.hhge0.mongodb.net/bg-auth-db?retryWrites=true&w=majority'
+  };
